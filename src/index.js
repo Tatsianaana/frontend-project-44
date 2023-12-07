d80f1a209ea50e5e01393373f965bf7f187d03cb
Tatsianaana <kazachaya.tatyana@mail.ru>
2023-12-07 18:29:03 +0300

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+const lauchGame = (rules, getAnswer) => {
+  console.log('Welcome to the Brain Games!');
+
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log(rules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [randomInt, rightAnswer] = getAnswer();
+
+    console.log(`Question: ${randomInt}`);
+
+    const usersAnswer = readlineSync.question('Your answer: ');
+
+    if (usersAnswer !== rightAnswer) {
+      console.log(`'${usersAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default lauchGame;
