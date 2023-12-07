7a270d19e5b82b573c76a6dc23efcb5711b68835
Tatsianaana <kazachaya.tatyana@mail.ru>
2023-12-07 18:30:53 +0300

    add games/game-even.js

---
+++ src/games/game-even.js
@@ -0,0 +1,18 @@
+import launchGame from '../index.js';
+
+const getRandomInt = (min, max) => {
+  const minIntNum = Math.ceil(min);
+  const maxIntNum = Math.floor(max);
+  return Math.floor(Math.random() * (maxIntNum - minIntNum + 1)) + minIntNum;
+};
+const task = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const getAnswer = () => {
+  const randomInt = getRandomInt(2, 20);
+  const rightAnswer = randomInt % 2 === 0 ? 'yes' : 'no';
+  return [String(randomInt), rightAnswer];
+};
+
+const runGame = () => launchGame(task, getAnswer);
+
+export default runGame;
