a0e8a8b85a0dbced55302dceb2c4c914c719d00c
Tatsianaana <kazachaya.tatyana@mail.ru>
2023-12-10 17:41:36 +0300

    update game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,10 +1,12 @@
 import app from '../index.js';
 import getRandomInt from '../half.js';
 
+const MIN_INT_NUM = 1;
+const MAX_INT_NUM = 20;
 const RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const getAnswer = () => {
-  const randomInt = getRandomInt(2, 20);
+  const randomInt = getRandomInt(MIN_INT_NUM, MAX_INT_NUM);
   const rightAnswer = randomInt % 2 === 0 ? 'yes' : 'no';
   return [String(randomInt), rightAnswer];
 };
