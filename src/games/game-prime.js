44c3680e36c8035202241c5b72bba0b357083baa
Tatsianaana <kazachaya.tatyana@mail.ru>
2023-12-10 18:21:39 +0300

    add game-prime.js

---
+++ src/games/game-prime.js
@@ -0,0 +1,31 @@
+import app from '../index.js';
+import getRandomInt from '../half.js';
+
+const MIN_INT_NUM = 1;
+const MAX_INT_NUM = 100;
+const RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  let divider = 2;
+  while (divider <= number / 2) {
+    if (number % divider === 0) {
+      return false;
+    }
+    divider += 1;
+  }
+  return true;
+};
+
+const getAnswer = () => {
+  const question = getRandomInt(MIN_INT_NUM, MAX_INT_NUM);
+  const rightAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return [String(question), rightAnswer];
+};
+
+const runGame = () => app(RULES, getAnswer);
+
+export default runGame;
