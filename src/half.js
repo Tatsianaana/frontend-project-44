d125f4918fba514bf518f3766be44b6d3b8fc294
Tatsianaana <kazachaya.tatyana@mail.ru>
2023-12-12 15:52:27 +0300

    deleted half.js

--- src/half.js
+++
@@ -1,7 +0,0 @@
-const getRandomInt = (min, max) => {
-  const minIntNum = Math.ceil(min);
-  const maxIntNum = Math.floor(max);
-  return Math.floor(Math.random() * (maxIntNum - minIntNum + 1)) + minIntNum;
-};
-
-export default getRandomInt;
